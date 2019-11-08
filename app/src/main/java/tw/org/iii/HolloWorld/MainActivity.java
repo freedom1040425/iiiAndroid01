9e4296518748d46355bae6bd0d50034671965dd2
Seraphim <freedom1040425@gmail.com>
2019-11-08 13:42:56 +0800

    HelloWorld first Android

--- app/src/main/java/tw/org/iii/HolloWorld/MainActivity.java
+++ app/src/main/java/tw/org/iii/HolloWorld/MainActivity.java
@@ -3,12 +3,31 @@ package tw.org.iii.HolloWorld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private Button buttonClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.lottery);
+        buttonClick = findViewById(R.id.click);
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("brad","OK");
+
+            }
+        });
+
     }
 }
